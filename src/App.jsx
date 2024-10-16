db6f769f15d5344f82f816a32aff6ba41c658ae9
hajarLafdaoui <155734398+hajarLafdaoui@users.noreply.github.com>
2024-10-16 22:01:36 +0100

    Add HTML structure for Expense Tracker with balance, history, and form components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header' 
+import Balance from './Components/Balance' 
+import IncomeExpenses from './Components/IncomeExpenses' 
+import TransactionList from './Components/TransactionList' 
+import AddTransaction from './Components/AddTransaction' 
+
 
 function App() {
   
   return (
     <>
     <Header/>
+    <div className="container">
+      <Balance/>
+      <IncomeExpenses/>
+      <TransactionList/>
+      <AddTransaction/>
+    </div>
     </>
   )
 }
---
+++ src/Components/Balance.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Balance = () => {
+  return (
+    <>
+    <h4>Your Balance </h4>
+    <p>$  0.00</p>
+      
+    </>
+  )
+}
+
+export default Balance
