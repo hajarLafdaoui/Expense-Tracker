2b3a38375c9ded17a5b7a1efe1d29906ff058204
hajarLafdaoui <155734398+hajarLafdaoui@users.noreply.github.com>
2024-11-08 23:43:35 +0100

    Deleting a transactions

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
-const transaction = ({transaction}) => {
-    const sign = transaction.amount > 0 ? '+' : '-';
+const Transaction = ({ transaction }) => {
+  const sign = transaction.amount > 0 ? '+' : '-';
+  const { deleteTransaction } = useContext(GlobalContext);
+
   return (
     <div>
-        <li className={transaction.amount > 0 ? 'plus' : 'minus'}>
-    {transaction.text} <span>{sign}$ {Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
-  </li> 
+      <li className={transaction.amount > 0 ? 'plus' : 'minus'}>
+        {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+        <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
+      </li>
     </div>
-  )
+  );
 }
 
-export default transaction
+export default Transaction;
