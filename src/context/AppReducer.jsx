adb009f35aa4b02c02e89b8b5467290bc85b7ee0
hajarLafdaoui <155734398+hajarLafdaoui@users.noreply.github.com>
2024-11-09 00:13:40 +0100

    Adding a transaction, and deleting the initialState

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -1,5 +1,11 @@
 export default (state, action) => {
     switch (action.type) {
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [...state.transactions, action.payload],
+            };
+            
         case 'DELETE_TRANSACTION':
             return {
                 ...state,
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -3,12 +3,7 @@ import AppReducer from './AppReducer'
 
 
 const initialState = {
-  transactions : [
-      { id: 1, text: 'Flower', amount: -20 },
-      { id: 2, text: 'Salary', amount: 300 },
-      { id: 3, text: 'Book', amount: -10 },
-      { id: 4, text: 'Camera', amount: 150 }
-    ]
+  transactions : []
 }
 
 export const GlobalContext = createContext(initialState)
@@ -23,10 +18,19 @@ function deleteTransaction(id) {
   })
 }
 
+function addTransaction(transaction) {
+  dispatch({
+    type: 'ADD_TRANSACTION',
+    payload: transaction
+  })
+}
+
   return (
     <GlobalContext.Provider value={{
        transactions: state.transactions,
-       deleteTransaction}}>
+       deleteTransaction,
+       addTransaction
+       }}>
       {children}
     </GlobalContext.Provider>
   );
