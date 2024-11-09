67c52c88560d9b614f4e7e03743ca2e9c76e2707
hajarLafdaoui <155734398+hajarLafdaoui@users.noreply.github.com>
2024-11-09 13:15:46 +0100

    fixing some errors

--- src/Components/IncomeExpenses.jsx
+++ src/Components/IncomeExpenses.jsx
@@ -12,10 +12,11 @@ const IncomeExpenses = () => {
   .reduce((acc, item) => (acc += item), 0)
   .toFixed(2);
 
-const expense = (
-  amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
-  -1
-).toFixed(2);
+  const expense = ( amounts
+  .filter(item => item < 0)
+  .reduce((acc, item) => (acc += item), 0) * -1)
+  .toFixed(2);
+
   return (
     <div className="inc-exp-container">
     <div>
